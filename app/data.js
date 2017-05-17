b06a0af5427d2269f8ccac1c1d3d1be45a5456b6
Ross Jones <ross@servercode.co.uk>
2017-05-17 16:59:15 +0100

    Trim down the data.js file by a small amount

--- app/data.js
+++ app/data.js
@@ -1,32 +1,4 @@
 module.exports = {
-  // Lists of organisations
-  organisations: [
-    {
-        title: "Cabinet Office",
-        name: "cabinet-office",
-        type: "national"
-    },
-    {
-        title: "Cheshire West and Chester",
-        name: "cheshire-west-and-chester",
-        type: "local"
-    },
-    {
-        title: "Wirral Council",
-        name: "wirral-council",
-        type: "local"
-    }
-  ],
-  local_organisations: function() {
-    return this.organisations.filter(function(o){
-        return o.type == 'local';
-    })
-  },
-  national_organisations: function() {
-    return this.organisations.filter(function(o){
-        return o.type == 'national';
-    })
-  },
   broadband: [
   {
     "title": "Broadband coverage 2013",
